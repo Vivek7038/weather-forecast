d4f310c20d83f2c752d6e28a5385f8849fbe8cf5
Vivek7038 <vivek888chavan@gmail.com>
2024-02-02 17:33:57 +0530

    minor error handing

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ import Search from "./components/Search.jsx";
 import ForecastCard from "./components/ForecastCard.jsx";
 import axios from "axios";
 import TemperatureToggle from "./components/TemperatureToggle.jsx";
-
-const api = {
-  key: import.meta.env.VITE_API_KEY,
-  base: "https://api.openweathermap.org/data/2.5/",
-};
+import api from "./helpers/api.js";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -17,12 +13,13 @@ const App = () => {
   const [error, setError] = useState("");
   const [isError, setIsError] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [weatherData, setWeatherData] = useState(null); 
-  const [unit, setUnit] = useState('metric'); // Default to Celsius
+  const [weatherData, setWeatherData] = useState(null);
+  const [unit, setUnit] = useState("metric"); // Default to Celsius
   const [fiveDayData, setFiveDayData] = useState([]);
   //to toggle between celsius and fahrenhiet
   const toggleUnit = () => {
-    setUnit(unit === 'metric' ? 'imperial' : 'metric');
+    setUnit(unit === "metric" ? "imperial" : "metric");
+
   };
   //to fetch 5 days forecast
   const fetchWeatherForecast = async (lat, lon) => {
@@ -30,61 +27,76 @@ const App = () => {
       const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api.key}`
       );
-  
+
       if (response.status !== 200) {
-        throw new Error(`Failed to fetch weather forecast. Status code: ${response.status}`);
+        throw new Error(
+          `Failed to fetch weather forecast. Status code: ${response.status}`
+        );
       }
-      setLoading(false)
+      setLoading(false);
       return response.data;
     } catch (error) {
-      console.error('Error fetching weather forecast:', error.message);
-      throw error; // Rethrow the error to handle it in the calling code
+      console.error("Error fetching weather forecast:", error.message);
+      setLoading(false);
+      throw error;
     }
   };
-  
+
   //to fetch 5 days details
   const getFiveDayAvgWeather = async (lat, lon, apiKey) => {
     try {
       const weatherForecastData = await fetchWeatherForecast(lat, lon, apiKey);
-      const dailyForecasts = weatherForecastData.list.reduce((acc, forecast) => {
-        const date = forecast.dt_txt.split(' ')[0];
-        if (!acc[date]) {
-          acc[date] = {
-            minTemp: forecast.main.temp_min,
-            maxTemp: forecast.main.temp_max,
-            count: 1,
-            weatherDescription: forecast.weather[0].description,
-            weatherIcon: forecast.weather[0].icon,
-          };
-        } else {
-          acc[date].minTemp = Math.min(acc[date].minTemp, forecast.main.temp_min);
-          acc[date].maxTemp = Math.max(acc[date].maxTemp, forecast.main.temp_max);
-          acc[date].count += 1;
-        }
-        return acc;
-      }, {});
-  
+      const dailyForecasts = weatherForecastData.list.reduce(
+        (acc, forecast) => {
+          const date = forecast.dt_txt.split(" ")[0];
+          if (!acc[date]) {
+            acc[date] = {
+              minTemp: forecast.main.temp_min,
+              maxTemp: forecast.main.temp_max,
+              count: 1,
+              weatherDescription: forecast.weather[0].description,
+              weatherIcon: forecast.weather[0].icon,
+            };
+          } else {
+            acc[date].minTemp = Math.min(
+              acc[date].minTemp,
+              forecast.main.temp_min
+            );
+            acc[date].maxTemp = Math.max(
+              acc[date].maxTemp,
+              forecast.main.temp_max
+            );
+            acc[date].count += 1;
+          }
+          return acc;
+        },
+        {}
+      );
+
       const calculateAverageTemperature = (minTemp, maxTemp) => {
         return (minTemp + maxTemp) / 2;
       };
 
       const fiveDayAvgWeather = Object.keys(dailyForecasts).map((date) => {
-        const { minTemp, maxTemp, count, weatherDescription, weatherIcon } = dailyForecasts[date];
+        const { minTemp, maxTemp, count, weatherDescription, weatherIcon } =
+          dailyForecasts[date];
         const avgTemperature = calculateAverageTemperature(minTemp, maxTemp);
         return { date, avgTemperature, weatherDescription, weatherIcon };
       });
-  
-      console.log('Five-day average weather:');
+
+      console.log("Five-day average weather:");
       console.log(fiveDayAvgWeather);
-      setFiveDayData(fiveDayAvgWeather)
+      setFiveDayData(fiveDayAvgWeather);
     } catch (error) {
-      console.error('Error getting five-day average weather:', error.message);
+      console.error("Error getting five-day average weather:", error.message);
+      setLoading(false);
+      return;
     }
   };
 
   // to fetch weather data based on city latitude and longitude
-  const fetchWeatherData = async (lat , lon) => {
-    setLoading(true)
+  const fetchWeatherData = async (lat, lon) => {
+    setLoading(true);
     try {
       const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api.key}&units=${unit}`
@@ -97,15 +109,18 @@ const App = () => {
       }
 
       const data = response.data;
-      const weatherForecastData = await fetchWeatherForecast(lat, lon)
-      const forecastdata=await getFiveDayAvgWeather(lat, lon,api.key);
+      const weatherForecastData = await fetchWeatherForecast(lat, lon);
+      const forecastdata = await getFiveDayAvgWeather(lat, lon, api.key);
       setWeatherData(data);
-      setSearchTerm("")
+      setSearchTerm("");
     } catch (error) {
-      console.error('Error fetching weather data:', error.message);
-      return 
+      console.error("Error fetching weather data:", error.message);
+      setLoading(false);
+      return;
     }
   };
+
+  //function to fetch latitude and longitude based on searched city
   const fetchLatLong = async () => {
     setLoading(true);
     try {
@@ -117,13 +132,17 @@ const App = () => {
       setLatitude(response.data[0].lat);
       setLongitude(response.data[0].lon);
       await fetchWeatherData(lat, lon);
-      
     } catch (error) {
-      setError(error.message); 
+      setError("No City found ,please  enter correct city name ");
+      setIsError(true);
       setLoading(false);
     }
   };
   function handleSearch() {
+    setIsError(false);
+    setError("");
+    setLatitude(null);
+    setLongitude(null);
     fetchLatLong();
   }
 
@@ -144,19 +163,25 @@ const App = () => {
           setSearchTerm={setSearchTerm}
           handleSearch={handleSearch}
         />
-      <TemperatureToggle unit={unit} toggleUnit={toggleUnit}/>
+       
+        <TemperatureToggle unit={unit} toggleUnit={toggleUnit} />
+      
         <div className="lg:flex sm:flex-row flex-col">
-        <WeatherCard
-         latitude={latitude}
-         longitude={longitude}
-         weatherData={weatherData}
-         unit={unit}
-         loading={loading}
-        />
-        <ForecastCard fiveDayData={fiveDayData} loading={loading} unit={unit}/>
+          {isError ? <h1>{error}</h1> : <></>}
+          <WeatherCard
+            latitude={latitude}
+            longitude={longitude}
+            weatherData={weatherData}
+            unit={unit}
+            loading={loading}
+          />
+          <ForecastCard
+            fiveDayData={fiveDayData}
+            loading={loading}
+            unit={unit}
+          />
         </div>
       </div>
-
     </div>
   );
 };
---
+++ src/helpers/api.js
@@ -0,0 +1,5 @@
+const api = {
+       key: import.meta.env.VITE_API_KEY,
+       base: "https://api.openweathermap.org/data/2.5/",
+     };
+     export default api;
